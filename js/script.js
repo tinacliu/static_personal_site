ff4d61038175a0ac99dfea00b4052ccc9076edad
Tina Liu <tina.chang.liu@gmail.com>
2019-08-13 17:09:25 +0100

    first version

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+  // display link when hover project card
+function mouseOver(elem) {
+  const cardHoverOver = elem.querySelector(".card-links");
+  cardHoverOver.style.display = "flex";
+}
+
+function mouseOut(elem) {
+  const cardHoverOver= elem.querySelector(".card-links");
+  cardHoverOver.style.display = "none";
+}
+
+  // display < when hover over menu items
+function changeText(text, elem) {
+  elem.innerText = text
+}
